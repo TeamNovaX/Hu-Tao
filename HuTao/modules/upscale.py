5931772fca7b0d145aa78bf988332961376b2a64
Yash Kedar <122774834+NyaNekko@users.noreply.github.com>
2023-08-27 00:03:29 +0530

    Update upscale.py

--- HuTao/modules/upscale.py
+++ HuTao/modules/upscale.py
@@ -1,14 +1,15 @@
 # Copyright 2023 Qewertyy, MIT License
 
 import os
+from pyrogram import filters
 from HuTao import app
 from HuTao.Config import COMMAND_HANDLER
-from HuTao.helper import getFile,UpscaleImages
+from HuTao.helpers import getFile, UpscaleImages
 
 @app.on_message(filters.command("upscale", COMMAND_HANDLER))
 async def upscaleImages(_,message):
     file = await getFile(message)
-    if fileId is None:
+    if file is None:
         return await message.reply_text("Replay to an image?")
     imageBytes = open(file,"rb").read()
     os.remove(file)
