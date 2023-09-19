ed873293cae3c9e9816248af89c29d3ca70fc389
Yash Kedar <122774834+NyaNekko@users.noreply.github.com>
2023-09-19 20:37:42 +0530

    Create __init__.py

---
+++ HuTao/database/__init__.py
@@ -0,0 +1,11 @@
+n#MIT License
+#Copyright (c) 2023, ©NovaNetworks
+
+from async_pymongo import AsyncClient
+
+from HuTao import DB_URL
+
+DBNAME = "HUTAO"
+
+mongo = AsyncClient(DB_URL)
+dbname = mongo[DBNAME]
